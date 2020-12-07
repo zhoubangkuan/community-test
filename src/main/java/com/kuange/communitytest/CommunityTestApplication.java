4ddc6d22753a8588b951b14e4a38db963a9686fb
laozhou <1355344418@qq.com>
2020-12-07 21:57:34 +0800

    第一次提交

---
+++ src/main/java/com/kuange/communitytest/CommunityTestApplication.java
@@ -0,0 +1,13 @@
+package com.kuange.communitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuange/communitytest/CommunityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuange.communitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
